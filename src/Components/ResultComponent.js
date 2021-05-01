a6ad5e8ec7416a6fa2c42247210bf0b96cc19ed7
jparashar <jayant.parashar@cognizant.com>
2021-05-02 01:57:09 +0530

    refactor : updated design

--- src/Components/ResultComponent.js
+++ src/Components/ResultComponent.js
@@ -9,7 +9,7 @@ const ResultComponent = ({ response = {}, errors = false, data: CONSTANTS = {} }
 
     return (
         <Fragment>
-            {(centers && centers.length) && <h2 className='container result-count'>{`${centers.length} ${CONSTANTS.RESULTS_FOUND}`}</h2>}
+            {(centers && !errors) && <h2 className='container result-count'>{`${centers.length} ${CONSTANTS.RESULTS_FOUND}`}</h2>}
             <div className='centers'>
                 {
                     (errors) ?
@@ -23,29 +23,26 @@ const ResultComponent = ({ response = {}, errors = false, data: CONSTANTS = {} }
                                 let _available_capacity = '', _min_age_limit = '', _vaccine = '';
                                 return (
                                     <div className='center' key={index}>
-                                        <div className='center__title'>
-                                            <h5 className='no-margin center__title-primary'><strong>{name}</strong></h5>
-                                            <h6 className='no-margin center__title-secondary'>{`${district_name}, ${state_name}`}</h6>
-                                        </div>
+
                                         <div className='center__tiles'>
                                             <Tile data={fee_type} />
                                             <Tile data={`${from}-${to}`} />
                                         </div>
+                                        <div className='center__title'>
+                                            <h5 className='no-margin center__title-primary'><strong>{name}</strong></h5>
+                                            <h6 className='no-margin center__title-secondary'>{`${district_name}, ${state_name}`}</h6>
+                                        </div>
                                         {
-                                            sessions.map((session = {}, idx = '') => {
+                                            sessions.forEach((session = {}) => {
                                                 const { available_capacity = '', min_age_limit = '', vaccine = '', date = '' } = session;
                                                 _available_capacity = _available_capacity || available_capacity;
                                                 _min_age_limit = _min_age_limit || min_age_limit;
                                                 _vaccine = _vaccine || vaccine;
-                                                return (
-                                                    <div className='center__cards' key={`${idx}__${index}`}>
-                                                    </div>
-                                                );
                                             })
                                         }
                                         <div className='card__age-limit'>
                                             <div>{`${_min_age_limit}+ age`}</div>
-                                            <div>{`${_available_capacity || 'not'} available`}</div>
+                                            <div>{`${_available_capacity.toFixed() || 'not'} available`}</div>
                                             <div>{`${_vaccine || 'coviShield'}`}</div>
                                         </div>
                                     </div>
