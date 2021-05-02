59aeab7703c7d70cce358749736c8685802c9af0
jparashar <jayant.parashar@cognizant.com>
2021-05-03 01:15:45 +0530

    refactor : tweaked api

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         .then(response => response.json())
         .then(res => {
           console.log(res.ip, 'ip');
-          axios.get(`//ip-api.com/json/${res.ip}`)
+          axios.get(`//ipapi.co/${res.ip}/json/`)
             .then(data => {
               console.log(data.data);
               const { data: { zip = '' } = {} } = data || {};
